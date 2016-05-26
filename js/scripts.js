28e0b234f0efd3cf232914fbef924fdda922a065
Stephen Emery and Zachary Richards <emery.stephen@gmail.com>
2016-05-25 19:32:04 -0700

    Add javascript interactivity to form, and font styling and background color

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
-  
+  $("#blanks form").submit(function(event) {
+
+    var name1Input = $("input#name").val();
+    var address1Input = $("input#address").val();
+    var state1Input = $("input#state").val();
+    var city1Input = $("input#city").val();
+    var zip1Input = $("input#zip").val();
+    var date1Input = $("input#date").val();
+
+    $(".name1").text(name1Input);
+    $(".address1").text(address1Input);
+    $(".state1").text(state1Input);
+    $(".city1").text(city1Input);
+    $(".zip1").text(zip1Input);
+    $(".date1").text(date1Input);
+
+    $("#letter").show();
+    $("#blanks").hide();
+
+    event.preventDefault();
+  });
 });
